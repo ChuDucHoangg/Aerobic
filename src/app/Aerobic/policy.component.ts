33c08dd6db9a5456090c2b1786d28a9cf3899daa
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2022-11-29 09:32:17 +0700

    upcodeAerobic

---
+++ src/app/Aerobic/policy.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+  selector:'policy',
+  templateUrl:'policy.component.html'
+})
+export  class PolicyComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,30 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from "./Aerobic/home.component";
 import {ClassComponent} from "./Aerobic/class.component";
 import {TeacherComponent} from "./Aerobic/teacher.component";
+import {BookComponent} from "./Aerobic/book.component";
+import {CourseComponent} from "./Aerobic/course.component";
+import {PolicyComponent} from "./Aerobic/policy.component";
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'class',component:ClassComponent},
+  {path:'teacher',component:TeacherComponent},
+  {path:'book',component:BookComponent},
+  {path:'course',component:CourseComponent},
+  {path:'policy',component:PolicyComponent},
 
 
+];
 
 @NgModule({
   declarations: [
-    AppComponent,HomeComponent,ClassComponent,TeacherComponent
+    AppComponent,HomeComponent,ClassComponent,TeacherComponent,BookComponent,CourseComponent,PolicyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
