f1633b63a9e31e147667af37107d1ae8aa8557ca
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2022-11-30 21:48:06 +0700

    upcodeAerobic

---
+++ src/app/Aerobic/tivi.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+  selector:'tivi',
+  templateUrl:'tivi.component.html'
+})
+export  class TiviComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TiviComponent} from "./Aerobic/tivi.component";
 import {HomeComponent} from "./Aerobic/home.component";
 import {ClassComponent} from "./Aerobic/class.component";
 import {TeacherComponent} from "./Aerobic/teacher.component";
 import {BookComponent} from "./Aerobic/book.component";
 import {CourseComponent} from "./Aerobic/course.component";
-import {PolicyComponent} from "./Aerobic/policy.component";
+import {ContactComponent,} from "./Aerobic/contact.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
+import {RouterOutlet} from "@angular/router";
 
 const appRoutes: Routes = [
-  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'tivi',component:TiviComponent},
   {path:'class',component:ClassComponent},
   {path:'teacher',component:TeacherComponent},
   {path:'book',component:BookComponent},
   {path:'course',component:CourseComponent},
-  {path:'policy',component:PolicyComponent},
+  {path:'contact',component:ContactComponent},
 
 
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,HomeComponent,ClassComponent,TeacherComponent,BookComponent,CourseComponent,PolicyComponent
+    AppComponent,HomeComponent,ClassComponent,TeacherComponent,BookComponent,CourseComponent,ContactComponent,TiviComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule, RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
